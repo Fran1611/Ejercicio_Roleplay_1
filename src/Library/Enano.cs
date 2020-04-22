b1f1121083815d251b7d06d3f9ae63b1c030fefd
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 17:55:23 -0300

    Update Enano.cs
    clase enano

--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -6,14 +6,21 @@ namespace Library
 
         private string name;
         private int life;
+        private List<Item> items;
 
-        public Midget(string name, int life) 
+        public Midget(string name, int life, List<Item> items ) 
  
         {
+            this.Items = items;
             this.Life = life;
             this.Name = name;
         }
         public string Name {get; set; }
         public int Life {get; set; }
+        
+        public string MidgetAttack(int otherDamage)
+        {
+            this.life = this.life - otherDamage;
+            return $"El Enano {this.name} fue atacado, su vida ahora es {this.life}\n";
     }
 }
