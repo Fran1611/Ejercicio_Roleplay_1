f7e325c7ad7074e014ea144ec1e4a4b8adbee961
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 10:18:22 -0300

    Update Funcionalidades.cs

--- src/Library/Funcionalidades.cs
+++ src/Library/Funcionalidades.cs
@@ -8,14 +8,14 @@ namespace Library
         {
             object resultado = itemsLista.SingleOrDefault(item => item.Daño == description);
             a.Vida = a.Vida - resultado.Daño;
-            Console.WriteLine("{0] ataca al personaje {1} ahora su vida es de {2}", a, b, a.Vida);
+            Console.WriteLine("{0] ataca al personaje {1} ahora su vida es de {2}", a.Nombre, b.Nombre, a.Vida);
         }
 
         public static void Cura(Personaje a, Personaje b, string description)
         {
             object resultado = itemsLista.SingleOrDefault(item => item.Daño == description);
             a.Vida = a.Vida + resultado.Cura;
-            Console.WriteLine("{0} esta curando al personaje {1} ahora su vida es de {2}", a, b, a.Vida);
+            Console.WriteLine("{0} esta curando al personaje {1} ahora su vida es de {2}", a.Nombre, b,Nombre, a.Vida);
         }
         
     }
