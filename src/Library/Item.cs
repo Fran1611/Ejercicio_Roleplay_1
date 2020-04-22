d978441d1e7040c8d813338e5c9239810e8bb96b
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 01:20:19 -0300

    Update Item.cs
    A la clase Items le agrege un metodo para agregar los objetos item a un arraylist

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Collections;
 
 namespace Library
 {
+    
     public class Item
     {
+        public static ArrayList itemsLista = new ArrayList();
         private int daño, defensa, curar;
         private string nombre; 
 
-        
-
+        public static void Main(string[] args)
+        {
+            AddItem("varita",7,3,2);
+            AddItem("piedra",9,0,0);
+        }
         public Item (string nombre, int daño, int defensa, int curar)
         {
             this.Nombre = nombre;
@@ -16,14 +22,16 @@ namespace Library
             this.Defensa = defensa;
             this.Curar = curar;
         }
-
+        public string Nombre {get; set; }
         public int Daño {get;set;}
         public int Defensa {get;set;}
         public int Curar {get;set;}
-        public string Nombre {get;set;}
 
-
-
+        public static void AddItem(string nombre, int daño, int defensa, int curar)
+        {
+            itemsLista.Add(new Library.Item (nombre, daño, defensa, curar));
+        }
+    
 
     }
 }
