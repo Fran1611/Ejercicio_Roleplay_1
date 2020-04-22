7a7dfbe2579c2329f48f2f924f17fe4980547d91
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 09:12:53 -0300

    Update Item.cs

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -6,15 +6,9 @@ namespace Library
     
     public class Item
     {
-        ArrayList itemsLista = new ArrayList();
         private int daño, defensa, curar;
         private string nombre; 
 
-        public static void Main(string[] args)
-        {
-            AddItem("varita",7,3,2);
-            AddItem("piedra",9,0,0);
-        }
         public Item (string nombre, int daño, int defensa, int curar)
         {
             this.Nombre = nombre;
@@ -26,12 +20,5 @@ namespace Library
         public int Daño {get;set;}
         public int Defensa {get;set;}
         public int Curar {get;set;}
-
-        public static void AddItem(string nombre, int daño, int defensa, int curar)
-        {
-            itemsLista.Add(new Library.Item (nombre, daño, defensa, curar));
-        }
-    
-
     }
 }
