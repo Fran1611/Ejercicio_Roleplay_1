d88a1fd0b718e4127c7553d88d069f257d384002
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 09:14:35 -0300

    Update Program.cs
    Agrego el metodo AddItem

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,6 +12,12 @@ namespace Program
             Library.Personaje zz = new Library.Personaje("El mago de Oz",7);
             Library.ConsolePrinter.TieneVida(hp);
             Library.ConsolePrinter.TieneVida(zz);
+            AddItem("piedra",7,0,0);
+        }
+        
+         public static void AddItem(string nombre, int daño, int defensa, int curar)
+        {
+            itemsLista.Add(new Library.Item (nombre, daño, defensa, curar));
         }
      
     }
