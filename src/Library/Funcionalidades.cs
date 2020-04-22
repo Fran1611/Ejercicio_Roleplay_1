db97ed31e665cd1df2500a0565c6f21ab402f99e
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 11:29:05 -0300

    Update Funcionalidades.cs
    Hola les muesto la clase Funciones, esta dando un problema, no reconoce la lista con los objetos de Items por lo tanto tambien genera un error en resultado.Daño y en resultado.Cura.
    De poder solucionar el reconocimiento creo que tendría que andar bien. saludos

--- src/Library/Funcionalidades.cs
+++ src/Library/Funcionalidades.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Library
 {
@@ -6,17 +7,16 @@ namespace Library
     {
         public static void Ataque(Personaje a, Personaje b, string description)
         {
-            object resultado = itemsLista.SingleOrDefault(item => item.Daño == description);
+            object resultado = itemsList.Find(x => x.Nombre.Contains(description));
             a.Vida = a.Vida - resultado.Daño;
             Console.WriteLine("{0] ataca al personaje {1} ahora su vida es de {2}", a.Nombre, b.Nombre, a.Vida);
         }
 
         public static void Cura(Personaje a, Personaje b, string description)
         {
-            object resultado = itemsLista.SingleOrDefault(item => item.Daño == description);
+            object resultado = itemsList.Find(x => x.Nombre.Contains(description));
             a.Vida = a.Vida + resultado.Cura;
             Console.WriteLine("{0} esta curando al personaje {1} ahora su vida es de {2}", a.Nombre, b.Nombre, a.Vida);
         }
-        
     }
 }
