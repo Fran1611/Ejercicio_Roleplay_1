23f28060a51b0cf992d0561391639180b78eb6ca
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 10:15:00 -0300

    Create Funcionalidades.cs

---
+++ src/Library/Funcionalidades.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Library
+{
+    public class Funciones
+    {
+        public static void Ataque(Personaje a, Personaje b, string description)
+        {
+            object resultado = itemsLista.SingleOrDefault(item => item.Daño == description);
+            a.Vida = a.Vida - resultado.Daño;
+            Console.WriteLine("{0] ataca al personaje {1} ahora su vida es de {3}", a, b, a.Vida);
+        }
+
+        public static void Cura(Personaje a, Personaje b, string description)
+        {
+            object resultado = itemsLista.SingleOrDefault(item => item.Daño == description);
+            a.Vida = a.Vida + resultado.Cura;
+            Console.WriteLine("{0} esta curando al personaje {1} ahora su vida es de {3}", a, b, a.Vida);
+        }
+        
+    }
+}
