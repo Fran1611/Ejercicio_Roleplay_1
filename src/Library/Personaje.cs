b312836a3e549bd5917057a79ad36e29ba82d7f6
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 15:56:04 -0300

    Update Personaje.cs
    Moví los métodos add y remove y la creación de la lista a una clase aparte

--- src/Library/Personaje.cs
+++ src/Library/Personaje.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Generic;
 namespace Library
 {
     public class Personaje
     {
-        List<Item> itemsLista = new List<Item>();
+
         private string nombre;
         private int vida;
 
@@ -16,15 +15,5 @@ namespace Library
         }
         public string Nombre {get; set; }
         public int Vida {get; set; }
-
-        public void AddItem(string nombre, int daño, int defensa, int curar)
-        {
-            itemsLista.Add(new Item(nombre,daño,defensa,curar));
-        }
-
-        public void RemoveItem(string nombre, int daño, int defensa, int curar) 
-        {
-            itemsLista.Remove(new Item() {Nombre = nombre, Daño = daño, Defensa = defensa, Curar = curar});
-        }
     }
 }
