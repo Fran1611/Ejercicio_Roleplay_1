77e0f4155509761b6a12896651b88d1a73d21804
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-04-22 21:35:32 -0300

    Update Dwarves.cs
    Info de los metodos y actualización de defensa

--- src/Library/Dwarves.cs
+++ src/Library/Dwarves.cs
@@ -23,11 +23,20 @@ namespace Library
         public int Life {get; set; }
         public List<Item> Items {get;set;}
         
-        public string DwarvesAttack(int otherDamage, int otherDefense)
+        //Este metodo Ataque se le pasa un numero que representa un daño que le ocasiona un personaje al otro.
+        //Se obtiene de una lista de objetos de la clase Item y se busca el atributo en este caso Daño.
+        //Luego se recalcula una nueva vida luego del ataque considerando que ese personaje puede tener un elemento de proteccion y
+        //retorna una actualizacion de su nueva vida.
+        
+        public string DwarvesAttack(int otherDamage)
         {
-            this.Life = this.Life + otherDefense - otherDamage;
+            this.Life = this.Life + this.Item[0].Defense - otherDamage;
             return $"El Enano {this.Name} fue atacado, su vida ahora es {this.Life}\n";
         }
+        
+        //En cuanto al metodo de curacion, es muy parecido a el de Ataque ya que accede a un valor int de una lista de objetos Item 
+        //buscando el atributo que corresponde a la curacion.
+        //Luego se calcula la vida del personaje luego de ser curado y retorna una actualizacion de su nueva vida.
 
         public string DwarvesCure(int cure)
         {
