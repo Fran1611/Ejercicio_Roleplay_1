0fa2e262db74296bd7097fda17d05884d07b208b
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-04-22 23:09:34 -0300

    Subo Test

--- src/Test/Library.Test/UnitTest1.cs
+++ src/Test/Library.Test/UnitTest1.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using System.Collections.Generic;
 namespace Library.Test
 {
     public class Tests
@@ -10,9 +10,23 @@ namespace Library.Test
         }
 
         [Test]
-        public void Test1()
+        // Test que comprueba que el ataque al Mago se realiza correctamente.
+        public void DwarvesAttackTest()
         {
-            Assert.Pass();
+            Item garrote = new Item("Garrote",26,0);
+            List<Item> armasElfo = new List<Item>();
+            armasElfo.Add(garrote);
+            Dwarves elfo = new Dwarves("Elfo de un ojo", 35, armasElfo);
+
+            Item paloMagico = new Item("Palo Magico",33,0);
+            List<Item> armasMago = new List<Item>();
+            armasMago.Add(paloMagico);
+            Wizards mago = new Wizards("Mago Negro",56,armasMago);
+
+            string attack = mago.WizardsAttack(elfo.Items[0].Damage);   
+            string expected = "El Mago Mago Negro fue atacado, su vida ahora es 30\n";
+
+            Assert.AreEqual(expected, attack);
         }
     }
 }
